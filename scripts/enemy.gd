9a422e169d1e7ab9ec6308223e1b3342af640050
corndogit <101812777+corndogit@users.noreply.github.com>
2024-12-21 14:28:27 +0000

    Initial commit

---
+++ scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var custom_sprite_frames: SpriteFrames = null
+
+
+func _ready() -> void:
+	if custom_sprite_frames:
+		$AnimatedSprite2D.sprite_frames = custom_sprite_frames
+	$AnimatedSprite2D.play()
+
+
+func stop():
+	$AnimatedSprite2D.stop()
---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+var is_dead = false
+const MOVE_SPEED = 15000.0
+
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
+
+func _process(delta: float) -> void:
+	if is_dead:
+		return
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * MOVE_SPEED * delta
+	move_and_slide()
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemies"):
+		get_tree().call_group("enemies", "stop")
+		die()
+
+
+func die():
+	is_dead = true
+	$AnimatedSprite2D.modulate = Color(1.0, 0.0, 0.0)
+	$AnimatedSprite2D.stop()
